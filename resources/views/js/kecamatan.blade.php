84e54a530d6ac033335e08116d766cc70dfd20ce
AnwarFauzi123 <anwar.fauzy18@gmail.com>
2021-05-31 18:10:18 +0700

    menyelesaikan kecamatan

---
+++ resources/views/js/kecamatan.blade.php
@@ -0,0 +1,88 @@
+<script type="text/javascript">
+function myMap() {
+        var marker;
+        function taruhMarker(peta, posisiTitik){
+            if( marker ){
+                // pindahkan marker
+                marker.setPosition(posisiTitik);
+            } else {
+                // buat marker baru
+                marker = new google.maps.Marker({
+                    position: posisiTitik,
+                    map: peta,
+                    draggable: true,
+                    animation: google.maps.Animation.BOUNCE
+                });
+            }
+                // isi nilai koordinat ke form
+        document.getElementById("lat").value = posisiTitik.lat();
+        document.getElementById("long").value = posisiTitik.lng();   
+        }
+        function initialize() {
+        var propertiPeta = {
+            center:new google.maps.LatLng(-7.21626538,107.9011917),
+            zoom:9,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+        
+        var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
+
+        google.maps.event.addListener(peta, 'click', function(event) {
+            taruhMarker(this, event.latLng);
+        });
+
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
+    }
+
+$(function () {
+ 
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });  
+    
+//Tabel Kecamatan
+    var tableKecamatan = $('.table-kecamatan').DataTable({
+        processing: true,
+        serverSide: true,
+        responsive: true,
+        autoWidth: false,
+        retrieve: true,
+        ajax: "",
+        columns: [
+            {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+            {data: 'nama', name: 'nama'},
+            {data: 'lat', name: 'lat'},
+            {data: 'long', name: 'long'},
+            {data: 'action', name: 'action', orderable: false, searchable: false},
+        ]
+    });
+
+//DELETE Kecamatan
+    $('body').on('click', '.deleteKecamatan', function (){
+        var kecamatan_id = $(this).data("id");
+        var result = Swal.fire({
+            title: 'Peringatan!', 
+            text: 'Apakah anda yakin?', 
+            icon: 'warning',
+            showCancelButton: true,
+        }).then((result) =>{
+                if (result.isConfirmed){
+                    $.ajax({
+                    type: "DELETE",
+                    url: "{{ route('kecamatan.store') }}"+'/'+kecamatan_id,
+                    success: function (data) {
+                        tableKecamatan.draw();
+                        Swal.fire("Sukes!", "Kecamatan Berhasil Dihapus!", "success");
+                    },
+                    error: function (data) {
+                        console.log('Error:', data);
+                    }
+                });
+            }
+        })
+    });
+});
+</script>
---
+++ resources/views/kecamatan/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('title')
+    <title>Kecamatan</title>
+@endsection
+
+@section('content')
+<div class="app-main__inner">
+    <div class="app-page-title">
+        <div class="page-title-wrapper">
+            <div class="page-title-heading">
+                <div class="page-title-icon">
+                    <i class="pe-7s-culture icon-gradient bg-mean-fruit"></i>
+                </div>
+                <div>Kecamatan di {{$kabupaten->nama}}
+                    <div class="page-title-subheading">This is an example dashboard created using build-in elements and components.
+                    </div>
+                </div>
+            </div>  
+        </div> 
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="mb-3 card">
+                <div class="card-header-tab card-header-tab-animation card-header">
+                    <div class="card-header-title">
+                    <a class="btn btn-success" href="{{route('kecamatan.create', $kabupaten->id)}}"><i class="metismenu-icon pe-7s-note"></i> Tambah Kecamatan</a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="tab-content">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover table-kecamatan">
+                                <thead>
+                                    <tr class="text-center">
+                                        <th width="15%">No</th>
+                                        <th width="20%">Kecamatan</th>
+                                        <th width="25%">Latitude</th>
+                                        <th width="25%">Longitude</th>
+                                        <th width="15%">Aksi</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>    
+    </div>
+</div>
+@include('js.kecamatan')      
+@endsection
